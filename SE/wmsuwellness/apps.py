7a01254d623749c5ade588a3223333dd21f289e5
Haney6262 <qb202101011@wmsu.edu.ph>
2024-11-11 13:40:33 +0800

    Initial commit

---
+++ SE/wmsuwellness/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WmsuwellnessConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wmsuwellness'
---
+++ SE/wmsuwellness/static/js/form.js
@@ -0,0 +1,6 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Registration submitted successfully!');
+    // Here you can add code to handle the form submission,
+    // such as sending the data to a server.
+});
